ab152aacc695036970d529e3da2e82f09e0e24fb
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2025-11-06 11:01:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './assets/components/header.jsx';
+import Footer from './assets/components/footer.jsx';
+import Button from './assets/components/button.jsx';
+
+const App = () => {
+  
+  
+  return(
+ <>
+ <Header />
+ <h2>Application
+
+ </h2>
+ <Button/>
+ 
+<Footer/>
+
+    </>
+  );
+  };export default App;
